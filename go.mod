609e975cc11dde429fefeba2fff7390df9e79d5e
Anatole <me@anatole.dev>
2022-05-14 11:09:26 +0200

    GQL - add `createCosmeticPaint` mutation

--- go.mod
+++ go.mod
@@ -22,8 +22,6 @@ require (
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
-	github.com/PuerkitoBio/purell v1.1.1 // indirect
-	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
---
+++ internal/gql/v3/resolvers/mutation/mutation.cosmetics.go
@@ -0,0 +1,79 @@
+package mutation
+
+import (
+	"context"
+	"time"
+
+	"github.com/SevenTV/Common/errors"
+	"github.com/SevenTV/Common/mongo"
+	"github.com/SevenTV/Common/structures/v3"
+	"github.com/SevenTV/Common/utils"
+	"github.com/seventv/api/internal/gql/v3/gen/model"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.uber.org/zap"
+)
+
+// CreateCosmeticPaint implements generated.MutationResolver
+func (r *Resolver) CreateCosmeticPaint(ctx context.Context, def model.CosmeticPaintInput) (primitive.ObjectID, error) {
+	mainColor := 0
+	if def.Color != nil {
+		mainColor = *def.Color
+	}
+	angle := 90
+	if def.Angle != nil {
+		angle = *def.Angle
+	}
+	shape := ""
+	if def.Shape != nil {
+		shape = *def.Shape
+	}
+	imgURL := ""
+	if def.ImageURL != nil {
+		imgURL = *def.ImageURL
+	}
+
+	stops := make([]structures.CosmeticPaintGradientStop, len(def.Stops))
+	for i, st := range def.Stops {
+		stops[i] = structures.CosmeticPaintGradientStop{
+			At:    st.At,
+			Color: int32(st.Color),
+		}
+	}
+
+	shadows := make([]structures.CosmeticPaintDropShadow, len(def.Shadows))
+	for i, sh := range def.Shadows {
+		shadows[i] = structures.CosmeticPaintDropShadow{
+			OffsetX: sh.XOffset,
+			OffsetY: sh.YOffset,
+			Radius:  sh.Radius,
+			Color:   int32(sh.Color),
+		}
+	}
+
+	cos := structures.Cosmetic[structures.CosmeticDataPaint]{
+		ID:       primitive.NewObjectIDFromTimestamp(time.Now()),
+		Kind:     structures.CosmeticKindNametagPaint,
+		Priority: 0,
+		Name:     def.Name,
+		Data: structures.CosmeticDataPaint{
+			Function:    structures.CosmeticPaintFunction(def.Function),
+			Color:       utils.PointerOf(int32(mainColor)),
+			Stops:       stops,
+			Repeat:      def.Repeat,
+			Angle:       int32(angle),
+			Shape:       shape,
+			ImageURL:    imgURL,
+			DropShadows: shadows,
+		},
+	}
+
+	result, err := r.Ctx.Inst().Mongo.Collection(mongo.CollectionNameCosmetics).InsertOne(ctx, cos)
+	if err != nil {
+		zap.S().Errorw("failed to create new paint cosmetic",
+			"error", err,
+		)
+		return primitive.NilObjectID, errors.ErrInternalServerError().SetDetail(err.Error())
+	}
+
+	return result.InsertedID.(primitive.ObjectID), nil
+}
