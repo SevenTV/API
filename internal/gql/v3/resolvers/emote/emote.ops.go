d4390a9d828f26b09cc4bb181d30f09ca56de262
Anatole <me@anatole.dev>
2022-06-24 17:55:26 +0200

    Rerun processing command

--- internal/gql/v3/resolvers/emote/emote.ops.go
+++ internal/gql/v3/resolvers/emote/emote.ops.go
@@ -2,6 +2,10 @@ package emote
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"path"
+	"time"
 
 	"github.com/seventv/api/internal/events"
 	"github.com/seventv/api/internal/gql/v3/auth"
@@ -10,10 +14,15 @@ import (
 	"github.com/seventv/api/internal/gql/v3/helpers"
 	"github.com/seventv/api/internal/gql/v3/types"
 	"github.com/seventv/common/errors"
+	"github.com/seventv/common/mongo"
 	"github.com/seventv/common/structures/v3"
 	"github.com/seventv/common/structures/v3/mutations"
+	"github.com/seventv/common/svc/s3"
 	"github.com/seventv/common/utils"
+	"github.com/seventv/image-processor/go/task"
+	messagequeue "github.com/seventv/message-queue/go"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.uber.org/zap"
 )
 
 type ResolverOps struct {
@@ -22,6 +31,88 @@ type ResolverOps struct {
 
 func NewOps(r types.Resolver) generated.EmoteOpsResolver {
 	return &ResolverOps{r}
+}
+
+func (r *ResolverOps) Rerun(ctx context.Context, obj *model.EmoteOps) (*model.Emote, error) {
+	// Get the emote
+	emote, err := r.Ctx.Inst().Loaders.EmoteByID().Load(obj.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	ver, verIndex := emote.GetVersion(emote.ID)
+	if verIndex == -1 {
+		return nil, errors.ErrUnknownEmote()
+	}
+	// Update the emote's lifecycle
+	eb := structures.NewEmoteBuilder(emote)
+
+	// Send the task
+	filekey := ver.InputFile.Key
+	taskData, err := json.Marshal(task.Task{
+		ID:    emote.ID.Hex(),
+		Flags: task.TaskFlagALL,
+		Input: task.TaskInput{
+			Bucket: r.Ctx.Config().S3.InternalBucket,
+			Key:    filekey,
+		},
+		Output: task.TaskOutput{
+			Prefix:       r.Ctx.Inst().S3.ComposeKey("emote", path.Dir(ver.InputFile.Key)),
+			Bucket:       r.Ctx.Config().S3.PublicBucket,
+			ACL:          *s3.AclPublicRead,
+			CacheControl: *s3.DefaultCacheControl,
+		},
+		SmallestMaxWidth:  96,
+		SmallestMaxHeight: 32,
+		Scales:            []int{1, 2, 3, 4},
+		ResizeRatio:       task.ResizeRatioNothing,
+		Limits: task.TaskLimits{
+			MaxProcessingTime: time.Duration(r.Ctx.Config().Limits.Emotes.MaxProcessingTimeSeconds) * time.Second,
+			MaxFrameCount:     r.Ctx.Config().Limits.Emotes.MaxFrameCount,
+			MaxWidth:          r.Ctx.Config().Limits.Emotes.MaxWidth,
+			MaxHeight:         r.Ctx.Config().Limits.Emotes.MaxHeight,
+		},
+	})
+
+	if err == nil {
+		err = r.Ctx.Inst().MessageQueue.Publish(ctx, messagequeue.OutgoingMessage{
+			Queue:   r.Ctx.Config().MessageQueue.ImageProcessorJobsQueueName,
+			Headers: messagequeue.MessageHeaders{},
+			Flags: messagequeue.MessageFlags{
+				ID:          emote.ID.Hex(),
+				ContentType: "application/json",
+				ReplyTo:     r.Ctx.Config().MessageQueue.ImageProcessorResultsQueueName,
+				Timestamp:   time.Now(),
+				RMQ: messagequeue.MessageFlagsRMQ{
+					DeliveryMode: messagequeue.RMQDeliveryModePersistent,
+				},
+				SQS: messagequeue.MessageFlagsSQS{},
+			},
+			Body: taskData,
+		})
+	}
+
+	if err != nil {
+		zap.S().Errorw("failed to marshal task",
+			"error", err,
+		)
+
+		return nil, errors.ErrInternalServerError()
+	}
+
+	eb.Update.Set(fmt.Sprintf("versions.%d.state.lifecycle", verIndex), structures.EmoteLifecycleProcessing)
+
+	if _, err = r.Ctx.Inst().Mongo.Collection(mongo.CollectionNameEmotes).UpdateOne(ctx, bson.M{
+		"versions.id": emote.ID,
+	}, eb.Update); err != nil {
+		zap.S().Errorw("mongo, failed to update lifecycle of emote pending processing job rerun")
+	}
+
+	if err = r.Ctx.Inst().Redis.RawClient().Publish(ctx, fmt.Sprintf("events:sub:emotes:%s", emote.ID.Hex()), "1").Err(); err != nil {
+		return nil, errors.ErrInternalServerError()
+	}
+
+	return helpers.EmoteStructureToModel(emote, r.Ctx.Config().CdnURL), nil
 }
 
 func (r *ResolverOps) Update(ctx context.Context, obj *model.EmoteOps, params model.EmoteUpdate) (*model.Emote, error) {
