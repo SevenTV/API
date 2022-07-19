d8a0fd4566ed3f26d302be4c504928d245242b9f
Anatole <me@anatole.dev>
2022-07-19 06:43:17 +0200

    move luascript

--- internal/limiter/limiter.go
+++ internal/limiter/limiter.go
@@ -2,13 +2,11 @@ package limiter
 
 import (
 	"context"
-	"os"
 	"sync"
 	"time"
 
 	"github.com/seventv/api/internal/gql/v3/auth"
 	"github.com/seventv/common/redis"
-	"github.com/seventv/common/utils"
 	"go.uber.org/zap"
 )
 
@@ -57,12 +55,32 @@ func (inst *limiterInst) LoadScript(ctx context.Context) error {
 	inst.mx.Lock()
 	defer inst.mx.Unlock()
 
-	script, err := os.ReadFile("./internal/limiter/limiter.lua")
-	if err != nil {
-		return err
-	}
+	var err error
 
-	inst.script, err = inst.redis.RawClient().ScriptLoad(ctx, utils.B2S(script)).Result()
+	inst.script, err = inst.redis.RawClient().ScriptLoad(ctx, `
+		local key = ARGV[1]
+		local expire = tonumber(ARGV[2])
+		local limit = tonumber(ARGV[3])
+		local by = tonumber(ARGV[4])
+		
+		local exists = redis.call("EXISTS", key)
+		
+		local count = redis.call("INCRBY", key, by)
+		
+		if exists == 0 then
+			redis.call("EXPIRE", key, expire)
+			return {count, expire}
+		end
+		
+		local ttl = redis.call("TTL", key)
+		
+		if count > limit then
+			return {redis.call("DECRBY", key, by), ttl, 1}
+		end
+		
+		return {count, ttl, 0}
+		
+`).Result()
 	if err != nil {
 		return err
 	}
