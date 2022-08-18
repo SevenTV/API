3a35803f14a3f54ada04463ae711d272fe9d5c51
Nerixyz <nerixdev@outlook.de>
2022-08-18 10:32:25 +0200

    refactor: require users to specify a connection platform

--- internal/loaders/user.loader.go
+++ internal/loaders/user.loader.go
@@ -42,16 +42,62 @@ func userLoader[T comparable](ctx context.Context, x inst, keyName string) *data
 					case "username":
 						v, _ := utils.ToAny(u.Username).(T)
 						m[v] = u
-					case "connection.id":
-						for _, c := range u.Connections {
-							if c.ID != "" {
-								v, _ := utils.ToAny(c.ID).(T)
-								m[v] = u
-							}
-						}
 					default:
 						v, _ := utils.ToAny(u.ID).(T)
 						m[v] = u
+					}
+				}
+
+				for i, v := range keys {
+					if x, ok := m[v]; ok {
+						items[i] = x
+					} else {
+						errs[i] = errors.ErrUnknownUser()
+					}
+				}
+			} else {
+				for i := range errs {
+					errs[i] = err
+				}
+			}
+
+			return items, errs
+		},
+	})
+}
+
+func userByConnectionLoader(ctx context.Context, x inst, platform structures.UserConnectionPlatform) *dataloader.DataLoader[string, structures.User] {
+	return dataloader.New(dataloader.Config[string, structures.User]{
+		Wait: time.Millisecond * 25,
+		Fetch: func(keys []string) ([]structures.User, []error) {
+			ctx, cancel := context.WithTimeout(ctx, time.Second*10)
+			defer cancel()
+
+			items := make([]structures.User, len(keys))
+			errs := make([]error, len(keys))
+
+			// Initially fill the response with deleted emotes in case some cannot be found
+			for i := 0; i < len(items); i++ {
+				items[i] = structures.DeletedUser
+			}
+
+			// Fetch users
+			result := x.query.Users(ctx, bson.M{
+				"connections.id":       bson.M{"$in": keys},
+				"connections.platform": platform,
+			})
+			if result.Empty() {
+				return items, errs
+			}
+			users, err := result.Items()
+
+			if err == nil {
+				m := make(map[string]structures.User)
+				for _, u := range users {
+					for _, c := range u.Connections {
+						if c.Platform == platform {
+							m[c.ID] = u
+						}
 					}
 				}
 
