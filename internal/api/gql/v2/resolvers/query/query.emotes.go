55906b267a09fafc00fd97a42bbc327f6829a87c
Anatole <me@anatole.dev>
2023-02-13 08:01:19 +0100

    fix: legacy global emotes

--- internal/api/gql/v2/resolvers/query/query.emotes.go
+++ internal/api/gql/v2/resolvers/query/query.emotes.go
@@ -2,17 +2,12 @@ package query
 
 import (
 	"context"
-	"strconv"
 
-	"github.com/seventv/api/data/query"
 	"github.com/seventv/api/internal/api/gql/v2/gen/model"
 	"github.com/seventv/api/internal/api/gql/v2/helpers"
-	"github.com/seventv/api/internal/api/gql/v3/auth"
 	"github.com/seventv/common/errors"
 	"github.com/seventv/common/structures/v3"
 	"github.com/seventv/common/utils"
-	"github.com/valyala/fasthttp"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -43,87 +38,26 @@ func (r *Resolver) SearchEmotes(
 	channel *string,
 	filterArg *model.EmoteFilter,
 ) ([]*model.Emote, error) {
-	actor := auth.For(ctx)
-
-	// Global State
-	filterDoc := bson.M{}
-
-	exactVersion := make(utils.Set[primitive.ObjectID])
-	modQueue := false
-
-	namedMap := map[primitive.ObjectID]string{}
-
-	if globalStateArg != nil && *globalStateArg != "include" {
-		set, err := r.Ctx.Inst().Query.GlobalEmoteSet(ctx)
-		if err == nil {
-			ids := make([]primitive.ObjectID, len(set.Emotes))
-			for i, ae := range set.Emotes {
-				ids[i] = ae.ID
-				namedMap[ae.ID] = ae.Name
-			}
-
-			switch *globalStateArg {
-			case "only":
-				filterDoc["versions.id"] = bson.M{"$in": ids}
-
-				exactVersion.Fill(ids...)
-			case "hide":
-				filterDoc["versions.id"] = bson.M{"$not": bson.M{"$in": ids}}
-			}
-		}
-	} else {
+	if globalStateArg == nil || *globalStateArg != "ONLY" {
 		return nil, errors.ErrInsufficientPrivilege().SetDetail("This endpoint is no longer available. Please use V3")
 	}
 
-	result, totalCount, err := r.Ctx.Inst().Query.SearchEmotes(ctx, query.SearchEmotesOptions{
-		Actor: &actor,
-		Limit: 50,
-		Filter: &query.SearchEmotesFilter{
-			Document: filterDoc,
-		},
-	})
+	set, err := r.Ctx.Inst().Query.GlobalEmoteSet(ctx)
 	if err != nil {
-		return nil, err
+		return nil, errors.ErrInsufficientPrivilege().SetDetail("This endpoint is no longer available. Please use V3")
 	}
 
-	models := make([]*model.Emote, len(result))
+	emoteIDs := utils.Map(set.Emotes, func(x structures.ActiveEmote) primitive.ObjectID {
+		return x.ID
+	})
 
-	for i, e := range result {
-		if len(e.Versions) > 0 {
-			foundExact := false
+	emotes, _ := r.Ctx.Inst().Loaders.EmoteByID().LoadAll(emoteIDs)
 
-			for _, ver := range e.Versions {
-				if name, ok := namedMap[ver.ID]; ok {
-					e.Name = name
-				}
+	result := make([]*model.Emote, len(emotes))
 
-				if !exactVersion.Has(ver.ID) {
-					continue
-				}
-
-				e.ID = ver.ID
-
-				foundExact = true
-
-				break
-			}
-
-			if !foundExact {
-				// Bring forward the latest version
-				ver := e.GetLatestVersion(!modQueue)
-				if !ver.ID.IsZero() {
-					e.ID = ver.ID
-				}
-			}
-		}
-
-		models[i] = helpers.EmoteStructureToModel(e, r.Ctx.Config().CdnURL)
+	for i, e := range emotes {
+		result[i] = helpers.EmoteStructureToModel(e, r.Ctx.Config().CdnURL)
 	}
 
-	rctx, _ := ctx.Value(helpers.RequestCtxKey).(*fasthttp.RequestCtx)
-	if rctx != nil {
-		rctx.Response.Header.Set("X-Collection-Size", strconv.Itoa(totalCount))
-	}
-
-	return models, nil
+	return result, nil
 }
