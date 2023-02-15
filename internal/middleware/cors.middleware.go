ae0bafe4123f35ac4c14dcb18df5450cae07bf00
Anatole <me@anatole.dev>
2023-02-15 07:15:39 +0100

    cors: allow v2 agent headers

--- internal/middleware/cors.middleware.go
+++ internal/middleware/cors.middleware.go
@@ -2,12 +2,24 @@ package middleware
 
 import (
 	"strconv"
+	"strings"
 
 	"github.com/seventv/api/internal/global"
 	"github.com/seventv/common/errors"
 	"github.com/seventv/common/utils"
 	"github.com/valyala/fasthttp"
 )
+
+var allowedHeaders = []string{
+	"Content-Type",
+	"Content-Length",
+	"Accept-Encoding",
+	"Authorization",
+	"Cookie",
+	"X-Emote-Data",
+	"X-SevenTV-Platform",
+	"X-SevenTV-Version",
+}
 
 func CORS(gctx global.Context) Middleware {
 	return func(ctx *fasthttp.RequestCtx) errors.APIError {
@@ -16,7 +28,7 @@ func CORS(gctx global.Context) Middleware {
 		allowCredentials := utils.Contains(gctx.Config().Http.Cookie.Whitelist, reqHost)
 
 		ctx.Response.Header.Set("Access-Control-Allow-Credentials", strconv.FormatBool(allowCredentials))
-		ctx.Response.Header.Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, Authorization, Cookie, X-Emote-Data")
+		ctx.Response.Header.Set("Access-Control-Allow-Headers", strings.Join(allowedHeaders, ", "))
 		ctx.Response.Header.Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
 		ctx.Response.Header.Set("Access-Control-Allow-Origin", reqHost)
 		ctx.Response.Header.Set("Vary", "Origin")
