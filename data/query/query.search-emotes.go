e60ede59dc3d24531a29d2b4e8e08af00ef13461
bread <75878656+broadeditz@users.noreply.github.com>
2023-11-30 21:33:52 +0100

    sort emote search results

--- data/query/query.search-emotes.go
+++ data/query/query.search-emotes.go
@@ -2,7 +2,9 @@ package query
 
 import (
 	"context"
+	"sort"
 	"strings"
+	"time"
 
 	"github.com/seventv/common/errors"
 	"github.com/seventv/common/structures/v3"
@@ -91,7 +93,63 @@ func (q *Query) SearchEmotes(ctx context.Context, opt SearchEmotesOptions) ([]st
 		return nil, 0, errors.ErrInternalServerError().SetDetail(err.Error())
 	}
 
-	return emotes, int(totalCount), nil
+	return sortEmoteSearchResults(emotes, req.Sort.By, req.Sort.Ascending), int(totalCount), nil
+}
+
+func sortEmoteSearchResults(emotes []structures.Emote, sortBy string, ascending bool) []structures.Emote {
+	if sortBy == "" || len(sortBy) == 0 {
+		return emotes
+	}
+
+	switch sortBy {
+	case "channel_count":
+		sort.Slice(emotes, func(i, j int) bool {
+			// get sum count of all versions per emote
+			var emote1Count int32
+			var emote2Count int32
+
+			for _, version := range emotes[i].Versions {
+				emote1Count += version.State.ChannelCount
+			}
+
+			for _, version := range emotes[j].Versions {
+				emote2Count += version.State.ChannelCount
+			}
+
+			if ascending {
+				return emote1Count < emote2Count
+			}
+
+			return emote1Count > emote2Count
+		})
+	case "created_at":
+		sort.Slice(emotes, func(i, j int) bool {
+			// find earliest created at data per emote
+			emote1CreatedAt := time.Now()
+			emote2CreatedAt := time.Now()
+
+			for _, version := range emotes[i].Versions {
+				// is this version older than the current earliest?
+				if emote1CreatedAt.Sub(version.CreatedAt) > 0 {
+					emote1CreatedAt = version.CreatedAt
+				}
+			}
+
+			for _, version := range emotes[j].Versions {
+				if emote2CreatedAt.Sub(version.CreatedAt) > 0 {
+					emote2CreatedAt = version.CreatedAt
+				}
+			}
+
+			if ascending {
+				return emote1CreatedAt.Before(emote2CreatedAt)
+			}
+
+			return emote1CreatedAt.After(emote2CreatedAt)
+		})
+	}
+
+	return emotes
 }
 
 type SearchEmotesOptions struct {
