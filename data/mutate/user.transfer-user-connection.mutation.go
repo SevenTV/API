0eb9379a1b02afdc43388dbebce12702d4cd2f1d
anatoleam <me@anatole.dev>
2023-12-06 10:09:22 +0100

    rest semantics for transfer user connection route

---
+++ data/mutate/user.transfer-user-connection.mutation.go
@@ -0,0 +1,34 @@
+package mutate
+
+import (
+	"context"
+
+	"github.com/seventv/common/errors"
+	"github.com/seventv/common/structures/v3"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.uber.org/zap"
+)
+
+func (m *Mutate) TransferUserConnection(ctx context.Context, transferer, transferee structures.User, connectionID string) error {
+	// Get connection from the outgoing user
+	connection, i := transferer.Connections.Get(connectionID)
+	if i == -1 {
+		return errors.ErrUnknownUserConnection()
+	}
+
+	// push connection to recipient
+	if _, err := m.mongo.Collection("users").UpdateOne(ctx, primitive.M{"_id": transferee.ID}, primitive.M{"$push": primitive.M{"connections": connection}}); err != nil {
+		zap.S().Errorw("mutate, TransferUserConnection(), couldn't push connection to transferee")
+
+		return errors.ErrInternalServerError()
+	}
+
+	// delete connection from donor
+	if _, err := m.mongo.Collection("users").UpdateOne(ctx, primitive.M{"_id": transferer.ID}, primitive.M{"$pull": primitive.M{"connections": connection}}); err != nil {
+		zap.S().Errorw("mutate, TransferUserConnection(), couldn't pull connection from transferer")
+
+		return errors.ErrInternalServerError()
+	}
+
+	return nil
+}
