18bc5a8de457b736df2a6c17f821224c6a42079a
anatoleam <me@anatole.dev>
2023-12-06 10:23:57 +0100

    remove reference to editors

--- data/mutate/user.delete.mutation.go
+++ data/mutate/user.delete.mutation.go
@@ -6,6 +6,7 @@ import (
 	"github.com/seventv/common/mongo"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.uber.org/zap"
 )
 
 func (m *Mutate) DeleteUser(ctx context.Context, userID primitive.ObjectID) (int, error) {
@@ -18,6 +19,15 @@ func (m *Mutate) DeleteUser(ctx context.Context, userID primitive.ObjectID) (int
 			return 0, err
 		}
 		docsDeletedCount += int(res.DeletedCount)
+	}
+
+	// Delete editor references
+	if _, err := m.mongo.Collection(mongo.CollectionNameUsers).UpdateMany(ctx, bson.M{
+		"editors.id": userID,
+	}, bson.M{
+		"$pull": bson.M{"editors": bson.M{"id": userID}},
+	}); err != nil {
+		zap.S().Errorw("mutate, DeleteUser(), failed to remove editor references", "error", err)
 	}
 
 	return docsDeletedCount, nil
